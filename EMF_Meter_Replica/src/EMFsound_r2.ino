f9d05b5a7c1ca659bfc858eb1de0cceff79c8bc3
JohnnyElectronic <johnnyelectronic1@gmail.com>
2026-04-13 09:25:54 -0700

    Updates and clean up of r2 code base. Has debug option and reset for DFplayer lockup Cleaned up debug print statements

--- EMF_Meter_Replica/src/EMFsound_r2.ino
+++ EMF_Meter_Replica/src/EMFsound_r2.ino
@@ -4,13 +4,17 @@ Supernatural EMF Meter Replica
 EMF ATTiny85 Code
 Release: 2.0 (Label: EMF / r2)
 
-
 Compatible with PCB version r-1 (based on modifications) using EMF_V1_2 define
 - 1.1 reduces the gap between high tone track play
 - 1.2 requires jumper between ATTiny85 pin 6 to DFPlayer pin 3
 
 Compatible with PCB version r-2 using EMF_V2_0 define
 - 2.0 New ATTiny85 pin mapping for r2 PCB
+- Updated audio file and added DFP reset code for DFP play lockup
+- Added BUSY PIN detect around EMF_TONE_LOW section
+- DFP Lockup only seems to be with the JL AC, AF... series so far. Could not create the issue with JL AB...,
+  TD5580 and DFROBOT versions. Noticed that DFRobot has a post play amp click. Need to rview further before
+  using in EMF design.
 
 Johnny Electronic
 https://github.com/JohnnyElectronic
@@ -54,7 +58,8 @@ DFPlayer - A Mini MP3 Player For Arduino
 //#define NANO
 #define ATTINY
 
-//#define EMF_V1_2        /* Enable version 1.2 code, stores changed volume level every 5s, reloads between power cycles, requires PCB r1 */
+// Comment out both EMF_V1_2 and EMF_V2_0 for early version <= 1.1
+//#define EMF_V1_2        /* Enable version 1.2 code, stores changed volume level every 3s, reloads between power cycles, requires PCB r1 */
 #define EMF_V2_0        /* Enable version 2.0 code, all features of 1.2 w/ new ATTiny 85 SOIC-8 pin mapping, requires PCB r2 */
 
 // Not all pins on the Mega and Mega 2560 boards support change interrupts, 
@@ -68,6 +73,7 @@ DFPlayer - A Mini MP3 Player For Arduino
 #define  DFP_BUSY_PIN  2     // Pin Assigned for busy detection, set to -1 if not used
 #define  SENSOR_PIN   A1     // EMF input level
 #define  METER_PIN    11     // PWM output to drive VU meter
+#define  DFP_LOCKUP    3     // Flags DFP lockup condition, used for LED
 #endif
 
 // ATtiny85 Pinouts
@@ -102,7 +108,7 @@ SoftwareSerial dfpSerial (DFP_RX_PIN, DFP_TX_PIN); // RX, TX
 
 /* Audio Tracks (mp3 folder on SD card):
 1- 0001_emf start                       0.238s
-2- 0002_emf low short                   0.282s
+2- 0002_emf low short2                  0.400s
 3- 0003_emf steady short (High Tone)    0.568s
 4- 0004_emf steady long (High Tone)     1.435s
 5- 0005_emf end                         0.282s
@@ -116,8 +122,8 @@ SoftwareSerial dfpSerial (DFP_RX_PIN, DFP_TX_PIN); // RX, TX
 #define EMF_POWER_UP     6
 
 // EMF States
-#define EMF_DETECT  0
-#define EMF_FOUND   1
+#define EMF_DETECT  0   // Idle or monitoring for EMF levels
+#define EMF_FOUND   1   // Found an EMF level
 
 /* EMF Levels (1.3v max scale) mapped to input voltage and LED indication
  LED   |  Voltage | Sensor Level
@@ -144,8 +150,10 @@ SoftwareSerial dfpSerial (DFP_RX_PIN, DFP_TX_PIN); // RX, TX
   1         0.60     125
   2         1.2      251
   3         1.8      380
-  4         2.4      504      
-  5         3.2      632
+            2.1      440
+  4         2.4      504
+                  
+  5         3.0      632
 */
 
 /* Set values for 1.5v, 3v, or 5v scale range */
@@ -154,18 +162,20 @@ SoftwareSerial dfpSerial (DFP_RX_PIN, DFP_TX_PIN); // RX, TX
 #define EMF_LEVEL_LOW   250    /* Set to 110 for 1.5v scale or 250 for 3v scale (Approximatley LED 2) */
 #define EMF_LEVEL_HIGH  440    /* Set to 200 for 1.5v scale or 440 for 3v scale (Approximatley LED 3 1/2) */
 
+#define EMF_TONE_LOW_DURATION      250     /* Maximum time until replay */   
 #define EMF_TONE_STEADYL_DURATION  1200    /* Maximum time until replay */   
 
 #define AUDIO_LEVEL_ADDR     0          // Memory address of EMF audio level setting
-#define EMF_AUDIO_LEVEL     20          // Typical 15-20 to start
-#define EMF_MAX_AUDIO_LEVEL 30          // Highest Level
-#define EMF_MIN_AUDIO_LEVEL  4          // Lowest Audible Level
-#define EMF_VOL_SAVE_DELAY   5000       // Check for save every 5s 
+#define EMF_AUDIO_LEVEL      20         // Typical 15-20 to start
+#define EMF_MAX_AUDIO_LEVEL  30         // Highest Level
+#define EMF_MIN_AUDIO_LEVEL  2          // Lowest Audible Level
+#define EMF_VOL_SAVE_DELAY   3000       // Check for save every 3s 
 byte emfVolumeLev = EMF_AUDIO_LEVEL;    // Current or initial DFP volume level for EMF
 
 const int sensorPin = SENSOR_PIN; // select the input pin for the VU level
 const int meterPin = METER_PIN;   // Output to VU meter
 const int emfDelay = 100;         // 100ms for default freq. Wait delay for checking EMF levels for audio only, 50-200 range, 200 decent but slows mid level sound.
+const int dfpInitDelay = 1200;    // Delay time needed for DFP player to initialize.
 const int meterMin =  20;         // Minimum level for meter to respond, shuts down PWM when below this level.
 
 int emfDetected = 0;              // Current state of EMF sound to signal detection
@@ -173,24 +183,26 @@ int sensorValue = 0;              // variable to store the value coming from the
 int meterOutputValue = 0;         // variable to store the converted meter value
 int lastSensorValue = 0;          // variable to store the last value coming from the sensor
 unsigned long emfTimer = 0;       // Delay timer
-unsigned long emfHighTimer = 0;   // Delay timer
-bool emfHighZone = false;         // Tracks if max EMF levels
+unsigned long emfMP3Timer = 0;    // Delay timer
+bool emfHighZone = false;         // Tracks MP3 timer state
+bool emfMidZone = false;          // Tracks MP3 timer state
 
 #if defined(DFP_DEBUG) || defined(EMF_V1_2) || defined(EMF_V2_0)
 int queryVal;
 #endif
 
-#if defined(EMF_V1_2) || defined(EMF_V2_0)
-unsigned long emfVolumeSaveTimer = 0;   // Delay timer
+#ifdef NANO
+int dfpLockupCnt = 0;     // Counts the number of resets so far
 #endif
 
-#ifdef EMF_DEBUG
-int lastMP3value = 0;      // variable to store the value of the MP3 played, debug only
+#if defined(EMF_V1_2) || defined(EMF_V2_0)
+unsigned long emfVolumeSaveTimer = 0;   // Volume save delay timer
 #endif
+
 
 void setup()
 {
-#ifdef DFP_DEBUG
+#if defined(DFP_DEBUG) || defined(EMF_DEBUG)
   Serial.begin(115200);
   Serial.println();
   Serial.println(F("DFPlayer Test"));
@@ -204,8 +216,8 @@ void setup()
 
 // Initial setup for DFPlayer.
 // Sets up DFP_BUSY_PIN if used to use the busy state/pin of the player. 
-// Resets the device and provides a 1 sec delay.
-    dfpSetup(DFP_BUSY_PIN, 1000);
+// Resets the device and provides a delay based on value provided.
+    dfpSetup(DFP_BUSY_PIN, dfpInitDelay);
 
 #if defined(EMF_V1_2) || defined(EMF_V2_0)   /* Save off volume levels between power cycles */
     emfVolumeLev = EEPROM.read(AUDIO_LEVEL_ADDR);
@@ -216,8 +228,8 @@ void setup()
     }
 #endif
 
-    dfpSetVolume(emfVolumeLev);
     dfpSetEq(DFP_EQ_CLASSIC);
+    dfpSetVolume(emfVolumeLev);
 
 #ifdef DFP_DEBUG
     queryVal = dfpReadQuery(0);
@@ -227,6 +239,11 @@ void setup()
     queryVal = dfpGetVolume();
     Serial.print(F("Params Vol:"));
     Serial.println(queryVal);
+#endif
+
+#ifdef NANO
+    pinMode(DFP_LOCKUP, OUTPUT);  // DFP play lockup LED
+    digitalWrite(DFP_LOCKUP, 0);  // turn the LED off
 #endif
 
   // Power up sound and flick meter for start up visual
@@ -249,9 +266,9 @@ void setup()
 
 void loop()
 {
-  sensorValue = analogRead(sensorPin);   // 0 - 1023
 
 #ifdef EMF_V1_2 
+  sensorValue = analogRead(sensorPin);   // 0 - 1023
   // Setup meter output. Scale the analog value, and write to the meter output pin based on the selected scale range (1.3v / 3v / 5v)
   meterOutputValue = sensorValue / 4;
   // meterOutputValue = map(sensorValue, 0, 632, 0, 255);  /* For 5v mapping */
@@ -264,22 +281,25 @@ void loop()
   }
 #endif
 
+    if (millis() - emfTimer > emfDelay) {
+
+#ifndef EMF_V1_2 
+      sensorValue = analogRead(sensorPin);   // 0 - 1023
+#endif
+
+
 #ifdef EMF_DEBUG
   if (sensorValue != lastSensorValue) {
-      Serial.print(F("Sen:"));
+      Serial.print(F("EMF Lvl:"));
       Serial.println(sensorValue);
 
-      Serial.print(F("Last Sen:"));
+      Serial.print(F("Last EMF Lvl:"));
       Serial.println(lastSensorValue);
 
       Serial.print(F("EMF Detect:"));
       Serial.println(emfDetected);
   }
-      Serial.print(F("MP3:"));
-      Serial.println(lastMP3value);
 #endif
-
-    if (millis() - emfTimer > emfDelay) {
 
       /* Checking for valid signal detected, startup */
       if (emfDetected == EMF_DETECT) {
@@ -287,24 +307,28 @@ void loop()
             emfDetected = EMF_FOUND;
             dfpPlayTrackMP3(EMF_TONE_START);
 
-    #ifdef EMF_DEBUG
-    lastMP3value = EMF_TONE_START;
-    #endif
-        } /* end if EMF_DETECT */
+#ifdef EMF_DEBUG
+    Serial.print(F("MP3:"));
+    Serial.println(EMF_TONE_START);
+#endif
+        } /* end if > EMF_LEVEL_LOW */
 
         emfHighZone = false;
+        emfMidZone = false;
       } else if (emfDetected == EMF_FOUND) {
         // Low detection state
         if (sensorValue <= EMF_LEVEL_LOW) {
           // Going back down
           dfpPlayTrackMP3(EMF_TONE_END);
 
-    #ifdef EMF_DEBUG
-    lastMP3value = EMF_TONE_END;
-    #endif
+#ifdef EMF_DEBUG
+        Serial.print(F("MP3:"));
+        Serial.println(EMF_TONE_END);
+#endif
 
           emfDetected = EMF_DETECT;  /* Reset back to detect state */
           emfHighZone = false;
+          emfMidZone = false;
         } else if ((sensorValue > EMF_LEVEL_LOW) && (sensorValue < EMF_LEVEL_HIGH)) {
           // Low steady detection state
           if (sensorValue >= lastSensorValue) {
@@ -313,32 +337,51 @@ void loop()
             sensorValue = analogRead(sensorPin);
             if (sensorValue >= lastSensorValue) {
               // Still mid range or higher
-              dfpPlayTrackMP3(EMF_TONE_LOW);
-            }
-          }
-          // Else we should be coming down in levels so let the next cycle catch it.
+                if (!emfMidZone) {
+                    emfMP3Timer = millis();
+                    emfMidZone = true;
+                }
 
-    #ifdef EMF_DEBUG
-    lastMP3value = EMF_TONE_LOW;
-    #endif
-    
+                // If not busy, play sound. Otherwise catch on next loop
+                if (digitalRead(DFP_BUSY_PIN) || ((millis() - emfMP3Timer) > EMF_TONE_LOW_DURATION))  {
+
+                  dfpPlayTrackMP3(EMF_TONE_LOW);
+                  emfMP3Timer = millis();
+#ifdef EMF_DEBUG
+                  Serial.print(F("EMF Lvl:"));
+                  Serial.println(sensorValue);
+                  Serial.print(F("Last EMF Lvl:"));
+                  Serial.println(lastSensorValue);
+                  Serial.print(F("MP3:"));
+                  Serial.println(EMF_TONE_LOW);
+#endif
+              } // end if DFP_BUSY_PIN
+            } // end if sensorValue, 2nd
+          } // end if sensorValue, 1st
+          // Else we should be coming down in levels so let the next cycle catch it.
           emfHighZone = false;
         } else {
           /* High detection state
             Check busy line - if still playing skip or if not play again 
             DFP_BUSY_PIN is active low on busy state */
             if (!emfHighZone) {
-                emfHighTimer = millis();
+                emfMP3Timer = millis();
                 emfHighZone = true;
+                emfMidZone = false;
             }
 
-            if (digitalRead(DFP_BUSY_PIN) || ((millis() - emfHighTimer) > EMF_TONE_STEADYL_DURATION))  {
+            if (digitalRead(DFP_BUSY_PIN) || ((millis() - emfMP3Timer) > EMF_TONE_STEADYL_DURATION))  {
                 // Replay just under sound file play length, slightly faster than using busy pin
                 dfpPlayTrackMP3(EMF_TONE_STEADYL);
-                emfHighTimer = millis();
+                emfMP3Timer = millis();
+
+#ifdef EMF_DEBUG
+        Serial.print(F("MP3:"));
+        Serial.println(EMF_TONE_STEADYL);
+#endif
             } // end if DFP_BUSY_PIN / Timer Check
         } // end if High Level
-      } // end if EMF_FOUND 
+      } // end if emfDetected 
       
       emfTimer = millis();
   } // End if timer triggered
@@ -353,24 +396,54 @@ void loop()
           //  delay (100);
           queryVal = dfpGetVolume();
 
-#ifdef DFP_DEBUG
+#if defined(DFP_DEBUG) || defined(EMF_DEBUG)
           Serial.print(F("Current Vol:"));
           Serial.println(queryVal);
 #endif
+
+          queryVal = dfpGetVolume();
           if ((queryVal != emfVolumeLev) && (queryVal <= EMF_MAX_AUDIO_LEVEL) && (queryVal >= EMF_MIN_AUDIO_LEVEL)) {
               /* Update value */
               emfVolumeLev = queryVal;
               EEPROM.write(AUDIO_LEVEL_ADDR, emfVolumeLev);
 
-#ifdef DFP_DEBUG
-          Serial.println(F("Updating EPROM"));
+#if defined(DFP_DEBUG) || defined(EMF_DEBUG)
+              Serial.println(F("Updating EPROM"));
 #endif
           }
-      }
+
+          /* Check for DFP in bad state and reset */
+          /* Seems to be issue with JL AF... chips */
+          queryVal = dfpGetStatus();
+          if (queryVal == 0x201) {
+              /* Player is in play mode, check busy pin and reset if not playing anything. */
+              if (digitalRead(DFP_BUSY_PIN)) {
+
+#ifdef NANO
+                  digitalWrite(DFP_LOCKUP, 1);  // Set lockup indicator
+                  dfpLockupCnt++;
+#endif
+
+#ifdef EMF_DEUG
+                  Serial.println(F("DFP State: LOCKUP"));
+                  Serial.print(F("DFP Lockup Cnt:"));
+                  Serial.println(dfpLockupCnt);
+                  Serial.println(F("RESET"));
+#endif
+                  dfpReset();
+                  delay(dfpInitDelay);
+                  dfpSetEq(DFP_EQ_CLASSIC);
+                  dfpSetVolume(emfVolumeLev);
+#ifdef NANO
+                  digitalWrite(DFP_LOCKUP, 0);  // Set lockup indicator
+#endif
+              } // end if DFP_BUSY_PIN
+          } // end if query
+      } // end if EMF_DETECT (Idle)
       emfVolumeSaveTimer = millis();
-  }
+  } // end if volume save
 #endif
 
   lastSensorValue = sensorValue; 
-}
+} // end main loop
 
